5bf840e1385aaed253d9dd7cf70048804521f47b
Kevin Huang <kevin9sd@gmail.com>
2024-03-05 17:05:03 -0500

    unary impl

--- rlox/src/parser.rs
+++ rlox/src/parser.rs
@@ -6,7 +6,7 @@ use std::iter::Peekable;
 pub fn parse(token_list: Vec<Token>) -> Expr {
     let mut iter = token_list.iter().peekable();
     
-    primary(&mut iter)
+    unary(&mut iter)
 }
 
 /**
@@ -32,6 +32,18 @@ fn comparison(iter: Peekable<std::slice::Iter<'_, Token>>) -> Expr {
 }
 
 
+fn unary(iter: &mut Peekable<std::slice::Iter<'_, Token>>) -> Expr {
+    let next_token = iter.peek().expect("Iterator should not be exhausted");
+
+    match next_token {
+        Token::MINUS | Token::BANG => 
+        {
+            Expr::Unary(iter.next().expect("We just peeked").clone(), Box::new(unary(iter)))
+        }
+        _ => primary(iter)
+    }
+}
+
 fn primary(iter: &mut Peekable<std::slice::Iter<'_, Token>>) -> Expr {
     let next_token = iter.next().expect("Iterator should not be exhausted").clone();
 
@@ -52,6 +64,22 @@ mod tests {
     use crate::parser::Expr;
     #[test]
     fn parse_tests() {
+        // terminals
         assert_eq!(parse(vec![Token::TRUE, Token::EOF]), Expr::Boolean(true));
+        assert_eq!(parse(vec![Token::FALSE, Token::EOF]), Expr::Boolean(false));
+        assert_eq!(parse(vec![Token::NIL, Token::EOF]), Expr::Nil);
+        assert_eq!(parse(vec![Token::STRING(String::from("blah")), 
+            Token::EOF]), Expr::String(String::from("blah")));
+        assert_eq!(parse(vec![Token::NUMBER(0.0), Token::EOF]), Expr::Number(0.0));
+
+        // unary 
+        assert_eq!(parse(vec![Token::BANG, Token::TRUE, Token::EOF]), 
+            Expr::Unary(Token::BANG, 
+                Box::new(Expr::Boolean(true))));
+
+        assert_eq!(parse(vec![Token::MINUS, Token::NUMBER(0.0), Token::EOF]), 
+            Expr::Unary(Token::MINUS, 
+                Box::new(Expr::Number(0.0))));
+
     }
 }
